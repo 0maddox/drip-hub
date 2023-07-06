e0a18b330d23f738131078424a97fb1282e14150
nicholas <nickkiim7@gmail.com>
2023-07-07 00:32:37 +0300

    added components

--- db/migrate/20230704222257_create_favourites.rb
+++ db/migrate/20230704222257_create_favourites.rb
@@ -1,6 +1,8 @@
 class CreateFavourites < ActiveRecord::Migration[7.0]
   def change
     create_table :favourites do |t|
+      t.references :user
+      t.references :cloth
 
       t.timestamps
     end
